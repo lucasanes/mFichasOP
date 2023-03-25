9628e37e143ea0f4223e3ed96314977f31b4d791
SkyLuke11 <sky.lucas003@gmail.com>
2023-03-24 23:11:49 -0300

    aaaa

--- src/layout/AppLayout/styles.js
+++ src/layout/AppLayout/styles.js
@@ -3,7 +3,6 @@ import { styled } from '../../stitches.config';
 export const Container = styled('div', {
   height: '100vh',
   display: 'flex',
-  position: 'fixed'
 })
 
 export const ContentWrapper = styled('main', {
