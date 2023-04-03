8caf90c69cd1cf3fdd41550075e819f6f34aae14
SkyLuke11 <sky.lucas003@gmail.com>
2023-04-03 11:50:27 -0300

    teste5

--- src/layout/AppLayout/styles.js
+++ src/layout/AppLayout/styles.js
@@ -3,24 +3,28 @@ import { styled } from '../../stitches.config';
 export const Container = styled('div', {
   height: '100vh',
   display: 'flex',
+  flex: 1
 })
 
 export const ContentWrapper = styled('main', {
   width: '100%',
-  marginBottom: '6rem',
+  marginTop: '6rem',
 
-  // variants: {
-  //   auth: {
-  //     true: {
-  //       paddingBottom: '8rem'
-  //     },
-  //     trueficha: {
-  //       paddingBottom: '6rem'
-  //     },
-  //     false: {
-  //       paddingBottom: '0rem'
-  //     },
-  //   }
-  // }
+  variants: {
+    auth: {
+      true: {
+        height: 'calc(100% - 140px)',
+        marginBottom: '8rem'
+      },
+      trueficha: {
+        height: 'calc(100% - 120px)',
+        marginBottom: '6rem'
+      },
+      false: {
+        height: '100%',
+        marginBottom: '0rem'
+      },
+    }
+  }
 
 })
