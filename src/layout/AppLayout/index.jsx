a4a249b5e5573cba3c9b1f68298bd8afd104c5f2
SkyLuke11 <sky.lucas003@gmail.com>
2023-04-06 22:49:16 -0300

    teste

--- src/layout/AppLayout/index.jsx
+++ src/layout/AppLayout/index.jsx
@@ -14,7 +14,7 @@ export function AppLayout() {
 
   window.addEventListener('click', (e) => {
     if (e.target.tagName == 'BUTTON') {
-      e.currentTarget.blur()
+      window.focus()
     }
   })
 
